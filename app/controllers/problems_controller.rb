7890a1eac48e311ebab4b539b7372d4aaf190d52
isseikz <issey.macky@gmail.com>
2016-07-15 22:43:34 +0900

    created problem curl views

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -69,6 +69,6 @@ class ProblemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def problem_params
-      params.require(:problem).permit(:theme_id_id, :type, :level, :content, :answer, :correct_counter)
+      params.require(:problem).permit(:theme_id, :category, :level, :content, :answer, :correct_counter)
     end
 end
--- app/views/problems/index.json.jbuilder
+++ app/views/problems/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@problems) do |problem|
-  json.extract! problem, :id, :theme_id_id, :type, :level, :content, :answer, :correct_counter
+  json.extract! problem, :id, :theme_id, :category, :level, :content, :answer, :correct_counter
   json.url problem_url(problem, format: :json)
 end
