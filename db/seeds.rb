43fd8f21b4e3643763400c66f59a92fde137de46
isseikz <issey.macky@gmail.com>
2016-07-21 01:43:26 +0000

    create sheets

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Problem.create!([
+  {theme_id: 1, category: 0, level: 3, content: "<p> \\(sin\\theta+cos\\theta=\\frac{1}{3}\\)のとき，次の式の値を求めよ.</p>\r\n                <ol>\r\n                  <li class=\"questions\">\\(sin\\theta cos\\theta,\\ sin^3\\theta + cos^3\\theta\\)</li>\r\n                  <li class=\"questions\">\\(sin \\theta - cos \\theta \\  (\\frac{\\pi}{2} < \\theta < \\pi ) \\)</li>\r\n                </ol>", answer: "<ol>\r\n                  <li>\\(sin\\theta cos\\theta=- \\frac{4}{9},\\ \\)\\(sin^3\\theta+cos^3\\theta=\\frac{13}{27}\\)</li>\r\n                  <li>\\(\\frac{\\sqrt{17}}{3}\\)</li>\r\n                </ol>", correct_counter: 0, section_id: 4}
+])
+Section.create!([
+  {subject_id: 1, number: 1, title: "式と証明"},
+  {subject_id: 1, number: 2, title: "複素数と方程式"},
+  {subject_id: 1, number: 3, title: "図形と方程式"},
+  {subject_id: 1, number: 4, title: "三角関数"}
+])
+Subject.create!([
+  {title: "数学2"}
+])
+Theme.create!([
+  {section_id: 4, number: 15, title: "一般角と三角関数"},
+  {section_id: 4, number: 16, title: "三角関数のグラフと応用"}
+])
+User.create!([
+  {first_name: "Issei", last_name: "Kuzumaki", email: "issey.macky@gmail.com", password_digest: "$2a$10$io6QwhjJO5UpP1YROrOfZuzTEFAXyi6sYmwzHh4fhmTrGKoUTla76"}
+])
